3b4a076029765183e602b7a1d020d28afc0ddde3
nguyentiendat20194504 <nguyentiendat080201@gmail.com>
2023-04-07 16:42:23 +0700

    fix event to repo pattern

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -1,9 +1,14 @@
 <?php
+
 namespace App\Repositories;
 
 use App\Models\Event;
+use App\Models\Coupon;
+use App\Models\Rank;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
-class EventRepository implements AbstractRepositoryInterface
+class EventRepository implements EventRepositoryInterface
 {
     public function show()
     {
@@ -19,13 +24,58 @@ class EventRepository implements AbstractRepositoryInterface
     }
     public function update($id, array $attributes)
     {
-        $event = Event::find($id);
-        $event->update($attributes);
-        return $event;
+        $coupon = Coupon::find($id);
+        $coupon->update($attributes);
+        return $coupon;
     }
     public function delete($id)
     {
         $event = Event::find($id);
         $event->delete();
+    }
+
+    public function getExchange()
+    {
+        return Coupon::where('point', '!=', 0)->get();
+    }
+
+    public function getCoupon()
+    {
+        return Coupon::where('point', 0)->get();
+    }
+    public function getRank()
+    {
+        return Rank::all();
+    }
+    public function getIdbyCoupon($coupon)
+    {
+        return Coupon::where('code', $coupon)->first()->id;
+    }
+    public function updatebyCoupon($coupon, $rank, $point)
+    {
+        Coupon::where('code', $coupon)->update(['rank' => $rank, 'point' => $point]);
+    }
+    public function getIdbyRank($rank)
+    {
+        $rank = Rank::where('rank', $rank)->first();
+        return $rank->id;
+    }
+
+    public function getRankPoint($id)
+    {
+        return Rank::where('id', $id)->first()->point;
+    }
+
+    public function getCouponofUser()
+    {
+        $rank_point = Auth::user()->rank_point;
+        $ranks = Rank::where('point', '<=', $rank_point)->pluck('id')->toArray();
+        $coupons = Coupon::whereIN('rank', $ranks)->get();
+        return $coupons;
+    }
+
+    public function updatePoint($user, $current_point)
+    {
+        User::where('id', $user->id)->update(['current_point' => $current_point]);
     }
 }
